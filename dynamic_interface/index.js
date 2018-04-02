9e6b614c9ae3fa149d304322187ffe91df313c80
ChienChia <hcc7391@gmail.com>
2018-04-02 22:08:33 +0800

    dynamic interface version 3

--- dynamic_interface/index.js
+++ dynamic_interface/index.js
@@ -1,3 +1,5 @@
+var TOOL;
+var box;
 $(document).ready(function(){
     $("#btn_restart").hide();
     $("#TOOL1").hide();
@@ -50,22 +52,64 @@ $(document).ready(function(){
             $("#tool3").css("left","10%").css("top","32%");
         }
     )
-
+    init();
     $("#tool1").click(
         function(){
             $("#TOOL1").show();
+            $("#TOOL3").hide();
+            //取得 id 為 "TOOL1" 物件，並且指定給 TOOL 變數
+            TOOL = document.getElementById("TOOL1");
+            //將 onclick 註冊給 pickup	
+            pickup();
         }
     )
     $("#tool2").click(
         function(){
+            $("#TOOL1").hide();
             $("#TOOL2").show();
+            $("#TOOL3").hide();
+            //取得 id 為 "TOOL2" 物件，並且指定給 TOOL 變數
+            TOOL = document.getElementById("TOOL2");
+            //將 onclick 註冊給 pickup	
+            pickup();
         }
-        
     )
     $("#tool3").click(
         function(){
+            $("#TOOL1").hide();
             $("#TOOL3").show();
+            //取得 id 為 "TOOL3" 物件，並且指定給 TOOL 變數
+            TOOL = document.getElementById("TOOL3");
+            //將 onclick 註冊給 pickup	
+            pickup();
         }
-        
     )
+
+    
 });
+
+
+
+/***********************************************************************************/
+function init(){
+}
+//撿起來
+function pickup(){
+    //設定滑鼠移動時， box 跟著移動
+    document.onmousemove = move;
+    //將onclick註冊給putdown所以再次按下滑鼠的時候，就是放下	
+    TOOL.onclick = putdown;
+}
+function move(e){
+    if(!e){
+        e = window.event;
+    }
+    TOOL.style.left = (e.clientX - 100) + "px";//若不加px，file fox會跑不起來。減5只是讓滑鼠留在方塊裡面
+    TOOL.style.top = (e.clientY - 100) + "px";
+}
+//放下
+function putdown(){
+    document.onmousemove = null;
+    TOOL.onclick = pickup;//更改事件處理器
+}
+/***********************************************************************************/
