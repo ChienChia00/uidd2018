214eb850620e0c0c627930a27a9f2cb83a137eeb
ChienChia <hcc7391@gmail.com>
2018-04-02 16:05:31 +0800

    dynamic interface version 2

---
+++ dynamic_interface/index.js
@@ -0,0 +1,71 @@
+$(document).ready(function(){
+    $("#btn_restart").hide();
+    $("#TOOL1").hide();
+    $("#TOOL2").hide();
+    $("#TOOL3").hide();
+    $("#btn_start").hover(
+        function(){
+            $("#btn_start").css("width","200px").css("height","200px");
+            $("#btn_start").css("left","45%").css("top","36%");
+        },
+        function(){
+            $("#btn_start").css("width","170px").css("height","170px");
+            $("#btn_start").css("left","46%").css("top","37%");
+        }
+    ); 
+    $("#btn_start").click(
+        function(){
+            $("#page_start").hide();
+            $("#btn_start").hide();
+        }
+    );
+
+    $("#tool1").hover(
+        function(){
+            $("#tool1").css("width","120px").css("height","120px");
+            $("#tool1").css("left","9.5%").css("top","3%");
+        },
+        function(){
+            $("#tool1").css("width","100px").css("height","100px");
+            $("#tool1").css("left","10%").css("top","4%");
+        }
+    )
+    $("#tool2").hover(
+        function(){
+            $("#tool2").css("width","120px").css("height","120px");
+            $("#tool2").css("left","9.5%").css("top","17%");
+        },
+        function(){
+            $("#tool2").css("width","100px").css("height","100px");
+            $("#tool2").css("left","10%").css("top","18%");
+        }
+    )
+    $("#tool3").hover(
+        function(){
+            $("#tool3").css("width","120px").css("height","120px");
+            $("#tool3").css("left","9.5%").css("top","31%");
+        },
+        function(){
+            $("#tool3").css("width","100px").css("height","100px");
+            $("#tool3").css("left","10%").css("top","32%");
+        }
+    )
+
+    $("#tool1").click(
+        function(){
+            $("#TOOL1").show();
+        }
+    )
+    $("#tool2").click(
+        function(){
+            $("#TOOL2").show();
+        }
+        
+    )
+    $("#tool3").click(
+        function(){
+            $("#TOOL3").show();
+        }
+        
+    )
+});
